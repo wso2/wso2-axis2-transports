0bc59fd68ef01d85fc48dd037b7c65f2e42545eb
Nuwan Wimalasekara <nuwanw@Nuwans-MacBook-Pro.local>
2016-05-11 10:30:00 +0530

    unsubscribe and subscribe when connection is lost

--- modules/mqtt/src/main/java/org/apache/axis2/transport/mqtt/MqttListenerCallback.java
+++ modules/mqtt/src/main/java/org/apache/axis2/transport/mqtt/MqttListenerCallback.java
@@ -41,6 +41,7 @@ public class MqttListenerCallback implements MqttCallback {
     public void connectionLost(Throwable throwable) {
         log.error("Connection Lost - Client Disconnected from topic "
                   + mqttEndpoint.getTopic(), throwable);
+        mqttEndpoint.unsubscribeFromTopic();
         mqttEndpoint.subscribeToTopic();
         /**
          * implements from MqttCallback
