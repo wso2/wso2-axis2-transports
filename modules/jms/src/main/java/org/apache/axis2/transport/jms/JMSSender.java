14514053fd0e03e02fbc3677b682dbbacf7577ee
msm1992 <madusha.sachini@yahoo.com>
2018-04-23 17:24:49 +0530

    Fix https://github.com/wso2/product-apim/issues/3236

--- modules/jms/src/main/java/org/apache/axis2/transport/jms/JMSSender.java
+++ modules/jms/src/main/java/org/apache/axis2/transport/jms/JMSSender.java
@@ -200,12 +200,23 @@ public class JMSSender extends AbstractTransportSender implements ManagementSupp
         msgCtx.setProperty(JMSConstants.PARAM_JMS_HYPHEN_MODE, hyphenSupport);
 
         // need to synchronize as Sessions are not thread safe
+        Destination replyDestination = null;
         synchronized (messageSender.getSession()) {
             try {
-                sendOverJMS(msgCtx, messageSender, contentTypeProperty, jmsConnectionFactory, jmsOut);
+                replyDestination = sendOverJMS(msgCtx, messageSender, contentTypeProperty, jmsConnectionFactory, jmsOut);
             } finally {
                 if (msgCtx.getProperty(JMSConstants.JMS_XA_TRANSACTION_MANAGER) == null) {
                     messageSender.close();
+                    if (replyDestination instanceof TemporaryQueue) {
+                        String temporaryQueueName = "";
+                        TemporaryQueue temporaryQueue = (TemporaryQueue) replyDestination;
+                        try {
+                            temporaryQueueName = temporaryQueue.getQueueName();
+                            temporaryQueue.delete();
+                        } catch (JMSException e) {
+                            log.error("Error while deleting the temporary queue " + temporaryQueueName, e);
+                        }
+                    }
                 }
             }
         }
@@ -214,10 +225,10 @@ public class JMSSender extends AbstractTransportSender implements ManagementSupp
     /**
      * Perform actual sending of the JMS message
      */
-    private void sendOverJMS(MessageContext msgCtx, JMSMessageSender messageSender,
+    private Destination sendOverJMS(MessageContext msgCtx, JMSMessageSender messageSender,
         String contentTypeProperty, JMSConnectionFactory jmsConnectionFactory,
         JMSOutTransportInfo jmsOut) throws AxisFault {
-        
+
         // convert the axis message context into a JMS Message that we can send over JMS
         Message message = null;
         String correlationId = null;
@@ -350,6 +361,7 @@ public class JMSSender extends AbstractTransportSender implements ManagementSupp
                 msgCtx, correlationId, contentTypeProperty);
             // TODO ********************************************************************************
         }
+        return replyDestination;
     }
 
     protected boolean isWaitForResponseOrReplyDestination(boolean waitForResponse, Destination replyDestination) {
