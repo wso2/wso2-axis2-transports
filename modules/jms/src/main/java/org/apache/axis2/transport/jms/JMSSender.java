249f79af4e8f26169e01bdc1564414edd18e3166
hasithajayasundara <hasithajayasundara1@gmail.com>
2018-10-24 16:03:42 +0530

    Resolve https://github.com/wso2/product-ei/issues/2776
    When receiving messages from IBM-MQ through a JMS proxy the Content-Type
    the header is getting removed. This causes to understand the message as
    a plain/text. Beacause of this the response message is wrapped around a
    CDATA tag. The commit resolves this issue.

--- modules/jms/src/main/java/org/apache/axis2/transport/jms/JMSSender.java
+++ modules/jms/src/main/java/org/apache/axis2/transport/jms/JMSSender.java
@@ -622,7 +622,7 @@ public class JMSSender extends AbstractTransportSender implements ManagementSupp
         // load any transport headers from received message
         JMSUtils.loadTransportHeaders(message, responseMsgCtx);
 
-        String contentType = contentTypeProperty == null ? null
+        String contentType = contentTypeProperty == outMsgCtx.getProperty(JMSConstants.CONTENT_TYPE) ? null
                 : JMSUtils.getProperty(message, contentTypeProperty);
 
         try {
